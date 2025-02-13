5c81c911781807f12d0f46f7c90a574f0e3c7c03
SY <SY@esuwainoMacBook-Air.local>
2025-02-13 20:02:19 +0900

    白黒を順番にターン

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [turnColor, setTurnColor] = useState(1);
+
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,13 +16,31 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const onClick = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard = structuredClone(board);
+    newBoard[y][x] = turnColor;
+    setBoard(newBoard);
+    if (turnColor === 1) {
+      setTurnColor(2);
+    } else {
+      setTurnColor(1);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`}>
-              <div className={styles.stone} />
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
             </div>
           )),
         )}
